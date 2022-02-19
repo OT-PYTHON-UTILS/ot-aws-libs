3a65a2f98774fb48af97ce46251c28a22d2bf1b0
Rajat vats <rajat.vats@opstree.com>
2022-02-19 13:33:52 +0530

    Fix error

--- otawslibs/aws_resource_tag_factory.py
+++ otawslibs/aws_resource_tag_factory.py
@@ -80,11 +80,11 @@ class getResoruceFinder:
 
     def _get_redis_ids_using_tags(self,tags):
 
-        redis = self.client.describe_replication_groups()
+        redis_client = self.client.describe_replication_groups()
         redis_instance_ids = []
 
         for redis in redis['ReplicationGroups']:
-            taglist = client.list_tags_for_resource(ResourceName=redis['ARN'])
+            taglist = redis_client.list_tags_for_resource(ResourceName=redis['ARN'])
             for tag in taglist['TagList']:
                 if tags == tag:
                     redis_instance_ids.append(redis['ReplicationGroupId'])
